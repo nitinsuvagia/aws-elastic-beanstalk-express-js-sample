a2f54bea0b4e33cfea8ec66d1b438d1fdaee95ea
hardikkanakSQT <74963082+hardikkanakSQT@users.noreply.github.com>
2022-04-04 10:02:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Team, CI/CD Pipeline setup successfully by NITIN'));
+app.get('/', (req, res) => res.send('Hello Team, CI/CD Pipeline setup successfully by NITIN and HARDIK'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
